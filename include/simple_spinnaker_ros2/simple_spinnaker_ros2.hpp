5685d3c5f46c59c30cc43ebefa8d010eed1a3051
Brach Knutson <brach2003@gmail.com>
2023-09-28 18:35:23 -0400

    initial driver code

---
+++ include/simple_spinnaker_ros2/simple_spinnaker_ros2.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <image_transport/image_transport.hpp>
+#include <Spinnaker.h>
+#include <SpinGenApi/SpinnakerGenApi.h>
+
+#define FEATURE_ENABLE (1)
+#define FEATURE_DISABLE (0)
+
+#define __DEBUG FEATURE_ENABLE
+
+class SpinnakerCamera {
+    public:
+    SpinnakerCamera(
+        image_transport::ImageTransport& it, 
+        const std::string& topic,
+        const std::string& frame,
+        Spinnaker::CameraList cameras, 
+        const std::string& serial,
+        std::shared_ptr<Spinnaker::ImageProcessor> postprocessor);
+    
+    void init();
+    void reInit();
+    void release();
+    void readAndPublish(rclcpp::Time now);
+
+    private:
+    const std::string 
+        topic, 
+        frame,
+        serial;
+    image_transport::Publisher pub;
+    Spinnaker::CameraPtr ptr;
+    Spinnaker::CameraList cameras;
+    std::shared_ptr<Spinnaker::ImageProcessor> postprocessor;
+};
